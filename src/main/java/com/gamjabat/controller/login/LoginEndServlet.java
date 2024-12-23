8e41f702fd93ce8c8028809453c310fe38dd6699
daineee424 <daineee424@gmail.com>
2024-12-23 09:56:14 +0900

    Feat: 회원가입 유효성 검사 로직 추가

---
+++ src/main/java/com/gamjabat/controller/login/LoginEndServlet.java
@@ -0,0 +1,42 @@
+package com.gamjabat.controller.login;
+
+import java.io.IOException;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import org.apache.ibatis.reflection.SystemMetaObject;
+
+/**
+ * Servlet implementation class LoginEndServlet
+ */
+@WebServlet("/login/loginend.do")
+public class LoginEndServlet extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+       
+    /**
+     * @see HttpServlet#HttpServlet()
+     */
+    public LoginEndServlet() {
+        super();
+        // TODO Auto-generated constructor stub
+    }
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		System.out.println("하이");
+	}
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		doGet(request, response);
+	}
+
+}
