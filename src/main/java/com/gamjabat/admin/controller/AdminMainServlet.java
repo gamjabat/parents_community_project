dc62a71177b67eb53a4052048b4a571e14a7fafb
omh1004 <ohchef2020@gmail.com>
2024-12-23 14:06:17 +0900

    Feat:세션추가밑관리자페이지코드수정

--- src/main/java/com/gamjabat/admin/controller/AdminMainServlet.java
+++ src/main/java/com/gamjabat/admin/controller/AdminMainServlet.java
@@ -1,7 +1,7 @@
 package com.gamjabat.admin.controller;
 
 import java.io.IOException;
-import java.util.ArrayList;
+import java.util.List;
 
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
@@ -9,9 +9,8 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
-import com.gamjabat.admin.model.dto.InqueryBoard;
 import com.gamjabat.admin.model.dto.Member;
-import com.gamjabat.admin.model.dto.ReportBoard;
+import com.gamjabat.admin.model.service.AdminMemberService;
 
 /**
  * Servlet implementation class AdminMainServlet
@@ -33,14 +32,21 @@ public class AdminMainServlet extends HttpServlet {
 	 */
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		
+		
+		AdminMemberService memberService = new AdminMemberService();
+		
+		
 		//db연동시, Service,dao, dto를 통해 db에서 데이터를 가져와 Attribute로 넘겨줄 예정
 		//우선 Member객체를 만들어서, ArrayList에 넣고,임의의 데이터를 넣어 화면 구현예정 
-		ArrayList<Member> m = new ArrayList<>();
-		ArrayList<ReportBoard> rbs = new ArrayList<>();
-		ArrayList<InqueryBoard> ibs = new ArrayList<>();
+		List<Member> m = memberService.selectMemberAll();
+//		ArrayList<ReportBoard> rbs = new ArrayList<>();
+//		ArrayList<InqueryBoard> ibs = new ArrayList<>();
 		
 		
-		String strDate = "20200806";
+		
+		
+		
+//		String strDate = "20200806";
 //		SimpleDateFormat dtFormat = new SimpleDateFormat("yyyyMMdd");
 //		SimpleDateFormat newDtFormat = new SimpleDateFormat("yyyy-MM-dd");
 //		// String 타입을 Date 타입으로 변환
@@ -54,56 +60,56 @@ public class AdminMainServlet extends HttpServlet {
 //		
 		
 		
-		
-		for(int i=1;i<=5;i++) {
-			Member m1 = Member.builder()
-					.memberNo("user"+i).memberId("userId"+i)
-					.name("이름"+i)
-					.address("우리")
-					.birthday(strDate)
-					.address("우리집!!!")
-					.email("asd3dasf@asdf.com")
-					.phone("102-213-2332")
-					.nickName("asdfa").createDate(strDate)
-					.updateDate(strDate).build();
-			m.add(m1);
-		}
-		
-		Member rf = Member.builder().memberId("user"+1).build();
-		Member sf = Member.builder().memberId("user"+2).build();
-		for(int i=1;i<=5;i++) {
-			
-			ReportBoard rb = ReportBoard.builder().reportNo(i+1+"")
-							.postNo(i+1+"")
-							.reportCode("댓글")
-							.reportTitle("탈주각"+i)
-							.reportContent("그때는 진짜 깡패가 되는거야!"+i)
-							.createDate("2024-01-01")
-							.reportStatus("처리중").reportMember(rf).suspectMember(sf)
-							.build();
-			rbs.add(rb);
-		}
-		
-		Member inqueryMember = Member.builder().memberId("user"+1).build();
-		for(int i=1;i<=5;i++) {
-			
-			InqueryBoard ib = InqueryBoard.builder().inqueryNo(i+1+"")
-							.postNo(i+1+"")
-							.inqueryCode("댓글")
-							.inqueryTitle("탈주각"+i)
-							.inqueryContent("그때는 진짜 깡패가 되는거야!"+i)
-							.createDate("2024-01-01")
-							.status("처리중").inqueryMember(inqueryMember)
-							.build();
-			ibs.add(ib);
-		}
-
-		
-		System.out.println(m.size());
-		
-		request.setAttribute("members", m);
-		request.setAttribute("reportboards", rbs);
-		request.setAttribute("inqueryboards", ibs);
+//		
+//		for(int i=1;i<=5;i++) {
+//			Member m1 = Member.builder()
+//					.memberNo("user"+i).memberId("userId"+i)
+//					.name("이름"+i)
+//					.address("우리")
+//					.birthday(strDate)
+//					.address("우리집!!!")
+//					.email("asd3dasf@asdf.com")
+//					.phone("102-213-2332")
+//					.nickName("asdfa").createDate(strDate)
+//					.updateDate(strDate).build();
+//			m.add(m1);
+//		}
+//		
+//		Member rf = Member.builder().memberId("user"+1).build();
+//		Member sf = Member.builder().memberId("user"+2).build();
+//		for(int i=1;i<=5;i++) {
+//			
+//			ReportBoard rb = ReportBoard.builder().reportNo(i+1+"")
+//							.postNo(i+1+"")
+//							.reportCode("댓글")
+//							.reportTitle("탈주각"+i)
+//							.reportContent("그때는 진짜 깡패가 되는거야!"+i)
+//							.createDate("2024-01-01")
+//							.reportStatus("처리중").reportMember(rf).suspectMember(sf)
+//							.build();
+//			rbs.add(rb);
+//		}
+//		
+//		Member inqueryMember = Member.builder().memberId("user"+1).build();
+//		for(int i=1;i<=5;i++) {
+//			
+//			InqueryBoard ib = InqueryBoard.builder().inqueryNo(i+1+"")
+//							.postNo(i+1+"")
+//							.inqueryCode("댓글")
+//							.inqueryTitle("탈주각"+i)
+//							.inqueryContent("그때는 진짜 깡패가 되는거야!"+i)
+//							.createDate("2024-01-01")
+//							.status("처리중").inqueryMember(inqueryMember)
+//							.build();
+//			ibs.add(ib);
+//		}
+//
+//		
+//		System.out.println(m.size());
+//		
+//		request.setAttribute("members", m);
+//		request.setAttribute("reportboards", rbs);
+//		request.setAttribute("inqueryboards", ibs);
 		
 		
 //		
--- src/main/java/com/gamjabat/controller/login/MainLoginServlet.java
+++ src/main/java/com/gamjabat/controller/login/MainLoginServlet.java
@@ -1,11 +1,15 @@
 package com.gamjabat.controller.login;
 
 import java.io.IOException;
+
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+
+import com.gamjabat.admin.model.dto.Member;
 
 /**
  * Servlet implementation class MainLoginServlet
@@ -26,6 +30,11 @@ public class MainLoginServlet extends HttpServlet {
 	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
 	 */
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		
+		
+		HttpSession session=((HttpServletRequest)request).getSession();
+		Member loginMember=(Member)session.getAttribute("loginMember");
+		
 		request.getRequestDispatcher("/").forward(request, response);
 	
 	}
