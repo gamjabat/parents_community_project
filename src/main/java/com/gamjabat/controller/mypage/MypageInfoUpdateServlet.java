1d05848f5ab238b7e64f418a63d8cfef6d20b728
daineee424 <daineee424@gmail.com>
2024-12-24 13:42:49 +0900

    Feat: 내 정보 수정의 비밀번호 재확인 구현

--- src/main/java/com/gamjabat/controller/mypage/MypageInfoUpdateServlet.java
+++ src/main/java/com/gamjabat/controller/mypage/MypageInfoUpdateServlet.java
@@ -1,11 +1,16 @@
 package com.gamjabat.controller.mypage;
 
 import java.io.IOException;
+
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+
+import com.gamjabat.common.PasswordEncoding;
+import com.gamjabat.model.dto.member.Member;
 
 /**
  * Servlet implementation class MypageInfoUpdateServlet
@@ -26,8 +31,27 @@ public class MypageInfoUpdateServlet extends HttpServlet {
 	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
 	 */
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		request.getRequestDispatcher("/WEB-INF/views/member/mypageInfoUpdate.jsp")
-		.forward(request, response);
+		request.setCharacterEncoding("utf-8");
+	
+		PasswordEncoding pe = new PasswordEncoding(request);
+		
+		String password = pe.getParameter("password");
+		
+		HttpSession session = request.getSession();
+		Member loginMember = (Member)session.getAttribute("loginMember");
+		
+		if(loginMember != null && password.equals(loginMember.getMemberPwd())) {
+			request.getRequestDispatcher("/WEB-INF/views/member/mypageInfoUpdate.jsp")
+			.forward(request, response);
+			
+		} else {
+			String msg, loc = "/member/mypagepwcheck.do";
+			msg = "비밀번호가 일치하지 않습니다. 다시 입력해주세요.";
+			request.setAttribute("msg", msg);
+			request.setAttribute("loc", loc);
+
+			request.getRequestDispatcher("/WEB-INF/views/common/msg.jsp").forward(request, response);
+		}
 	}
 
 	/**
