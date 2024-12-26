36374ec52f35aabd83b6a0f3aa0a58ce83f42787
omh1004 <ohchef2020@gmail.com>
2024-12-27 08:49:35 +0900

    Feat:신고등록기능추가전서블릿추가

---
+++ src/main/java/com/gamjabat/admin/controller/report/ReportCheckServlet.java
@@ -0,0 +1,56 @@
+package com.gamjabat.admin.controller.report;
+
+import java.io.IOException;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import com.gamjabat.admin.model.dto.ReportBoard;
+import com.gamjabat.admin.model.service.report.AdminReportService;
+
+/**
+ * Servlet implementation class ReportCheckServlet
+ */
+@WebServlet("/admin/reportCheck.do")
+public class ReportCheckServlet extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+       
+    /**
+     * @see HttpServlet#HttpServlet()
+     */
+    public ReportCheckServlet() {
+        super();
+        // TODO Auto-generated constructor stub
+    }
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+
+		
+		String reportNo = request.getParameter("reportNo");
+		
+		//신고글 정보 가져오기
+		ReportBoard reportBoard = new AdminReportService().selectReportBoardByNo(reportNo);
+		
+		
+		
+		
+	
+	
+	
+	}
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		doGet(request, response);
+	}
+
+}
